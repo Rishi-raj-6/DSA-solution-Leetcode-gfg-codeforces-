1dbdc3d5f682752515c698766d7a7d3d0d9745a5
RISHI RAJ <rajrishirr6122004@gmail.com>
2026-06-28 00:51:19 +0530

    [Syncode] LeetCode/Medium/Find the Maximum Number of Elements in Subset

---
+++ LeetCode/Medium/Find the Maximum Number of Elements in Subset/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        unordered_map<long long,int> freq;
+        for(int x:nums) freq[x]++;
+        int ans=1;
+        if(freq.count(1))
+            ans=max(ans,freq[1]&1?freq[1]:freq[1]-1);
+        for(auto &[x,_]:freq){
+            if(x==1) continue;
+            long long cur=x;
+            int len=0;
+            while(freq.count(cur)&&freq[cur]>=2){
+                len+=2;
+                if(cur>1000000000LL/cur) break;
+                cur*=cur;
+            }
+            if(freq.count(cur)&&freq[cur]==1) len++;
+            else len=max(1,len-1);
+            ans=max(ans,len);
+        }
+        return ans;
+    }
+};
